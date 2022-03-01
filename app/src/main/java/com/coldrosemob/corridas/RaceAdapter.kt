a0410c97ff8a62be01d57d7e0ad71ab86f110539
arthu <arthurguilhermesb@gmail.com>
2022-03-01 14:55:29 -0300

    implementation of viewBinding on RaceAdapter

--- app/src/main/java/com/coldrosemob/corridas/RaceAdapter.kt
+++ app/src/main/java/com/coldrosemob/corridas/RaceAdapter.kt
@@ -2,29 +2,29 @@ package com.coldrosemob.corridas
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.coldrosemob.corridas.databinding.RaceInfoBinding
 
 class RaceAdapter(private var raceList: List<Race>) : RecyclerView.Adapter<RaceAdapter.RaceViewHolder>() {
 
-    inner class RaceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val raceVehicle : TextView = itemView.findViewById(R.id.raceVehicle)
-        val raceDate : TextView = itemView.findViewById(R.id.raceDate)
-        val racePrice : TextView = itemView.findViewById(R.id.racePrice)
-    }
+    inner class RaceViewHolder(val binding : RaceInfoBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RaceViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.race_info, parent, false)
-        return RaceViewHolder(view)
+        val view = LayoutInflater.from(parent.context)
+        val binding = RaceInfoBinding.inflate(view, parent, false)
+        return RaceViewHolder(binding)
     }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: RaceViewHolder, position: Int) {
-        holder.raceVehicle.text = raceList[position].vehicle
-        holder.raceDate.text = raceList[position].date
-        holder.racePrice.text = "R$${raceList[position].price}"
+        holder.binding.apply {
+            raceVehicle.text = raceList[position].vehicle
+            raceDate.text = raceList[position].date
+            racePrice.text = "R$${raceList[position].price}"
+        }
     }
 
     override fun getItemCount(): Int = raceList.size
