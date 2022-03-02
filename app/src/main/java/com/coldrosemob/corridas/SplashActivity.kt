83305471efd47942b4efd0e063424b11b00bca28
arthu <arthurguilhermesb@gmail.com>
2022-03-01 22:28:06 -0300

    work on splash screen

--- app/src/main/java/com/coldrosemob/corridas/SplashActivity.kt
+++ app/src/main/java/com/coldrosemob/corridas/SplashActivity.kt
@@ -4,17 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import com.coldrosemob.corridas.view.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object{
+        private const val SPLASHTIME = 2000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.myLooper()!!).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
+
+        }, SPLASHTIME)
 
     }
 }
